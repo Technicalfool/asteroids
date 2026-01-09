5dfea23d8f059a89db92737e25a3248defba6784
Technicalfool <github@crystaldragon.co.uk>
2026-01-09 14:33:31 +0000

    bootdev asteroids course

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# from re import T
-
 import pygame
 
 import constants
@@ -12,7 +10,8 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dT = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -20,6 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dT = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
